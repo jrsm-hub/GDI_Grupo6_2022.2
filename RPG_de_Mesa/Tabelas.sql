10843e8cd6b20e863b60da733df2478f0c8979aa
GabrielJLuna <43001561+GabrielJLuna@users.noreply.github.com>
2023-04-18 23:56:22 -0300

    Update Tabelas.sql

--- RPG_de_Mesa/Tabelas.sql
+++ RPG_de_Mesa/Tabelas.sql
@@ -157,9 +157,10 @@ CREATE TABLE recompensas (
     cod_p VARCHAR2 (4),
     cod_m VARCHAR2 (4),
     cod_l VARCHAR2 (4),
+    data_hora DATE NOT NULL,
 
     CONSTRAINT recompensas_pkey PRIMARY KEY (id),
-    CONSTRAINT recompensas_fkey1 FOREIGN KEY (cod_p, cod_m, cod_l)  REFERENCES combate (cod_p, cod_m, cod_l)
+    CONSTRAINT recompensas_fkey1 FOREIGN KEY (cod_p, cod_m, cod_l, data_hora)  REFERENCES combate (cod_p, cod_m, cod_l, data_hora)
     
 );
 
